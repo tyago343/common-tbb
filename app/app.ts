4847e13cb0a6a3fc4fbc48f665059ddb98c809a9
tyago343 <santiago@tbb.agency>
2020-11-08 15:11:11 +0100

    Cambio en la version de sequelize

--- app/app.ts
+++ app/app.ts
@@ -5,9 +5,9 @@ const app: express.Application = express()
 app.get('/', function(req, res) {
     res.send('Hello world');
 })
-app.listen(3000, function() {
+app.listen(3000, async function() {
     console.log('Example app listening on port 3001!')
-    sequelize.authenticate().then(() => {
+    await sequelize.sync({force: true}).then(() => {
         console.log('hola nase de datos')
     }).catch(console.log)
 })
--- app/db/index.js
+++ app/db/index.js
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
-export const sequelize = new Sequelize("commonkey", "santiago3", "123456", {
-  host: "localhost",
+export const sequelize = new Sequelize({
+  database: "commonkey",
   dialect: "postgres",
+  username: "santiago3",
+  password: "123456",
+  storage: ":memory:",
   models: [__dirname + "/models"],
 });
