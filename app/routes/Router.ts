643feded8e27570899a015ec1a22e4b807530abc
tyago343 <tyagocasanova@gmail.com>
2020-12-09 02:06:17 +0100

    Rutas de usuario, falta modificacion para modularizarlas"

--- app/routes/Router.ts
+++ app/routes/Router.ts
@@ -2,12 +2,15 @@ import { UserController } from "../controllers/user.controller";
 
 export class Router {
   public userController: UserController = new UserController();
-
   public routes(app): void {
     app
       .route("/users")
       .get(this.userController.index)
       .post(this.userController.save);
-    app.route("/users/:id").put(this.userController.update);
+    app
+      .route("/users/:id")
+      .get(this.userController.getOne)
+      .put(this.userController.update)
+      .delete(this.userController.delete);
   }
 }
