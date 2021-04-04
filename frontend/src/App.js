30c5fea6e5039bbea9d9e6374112f9d19c5e8511
tyago343 <tyagocasanova@gmail.com>
2021-04-05 01:56:57 +0200

    cambios de logica buscando menos requests'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import { fetchEntries } from "./utils";
 
 const App = () => {
+  const [entries, setEntries] = useState([])
+  useEffect(() => {
+    const getEntries = async () => {
+      const entries = await fetchEntries();
+      if(entries) setEntries(entries);
+    }
+    getEntries();
+  }, []);
   return (
     <Fragment>
       <Header />
-      <Home />
+      <Home entries={entries} />
     </Fragment>
   );
 };
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,22 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { HomeWrapper } from "./styles";
-import { fetchEntries } from "./utils";
-const Home = () => {
-  const [entries, setEntries] = useState([])
-  useEffect(() => {
-    const getEntries = async () => {
-      const entries = await fetchEntries();
-      if(entries) setEntries(entries);
-    }
-    getEntries();
-  }, []);
+const Home = ({entries}) => {
+  
   return (
     <HomeWrapper>
       <section>
         <input />
       </section>
       <section>
-        <table>{entries && entries.map(enrty => <p>{enrty.application}</p>)}</table>
+        <table>{entries && entries.map(entry => <p>{entry.application}</p>)}</table>
       </section>
     </HomeWrapper>
   );
