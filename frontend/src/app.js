ebddfebe2594741c758c54121335e260b683bbbf
Santiago Casanova <santiago@tbb.agency>
2021-07-30 14:31:32 +0200

    Trabajo en las cosas de API

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { UnauthenticatedApp } from 'unauthenticated-app';
-import "./styles/global.css"
+import React from "react";
+import { UnauthenticatedApp } from "unauthenticated-app";
+import "./styles/global.css";
+import { client } from "utils/api";
 
 function App() {
-    function handleSubmit(formData){
-        
-    }
-    return (
-        <div>
-            <UnauthenticatedApp />
-        </div>
-    )
+  function handleSubmit(formData) {}
+  return (
+    <div>
+      <UnauthenticatedApp />
+    </div>
+  );
 }
-export {App}
+export { App };
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,13 +1,27 @@
-async function client(endpoint, {data, token, headers: customHeaders, ...customConfig} = {}) {
-    const config = {
-        method: data ? "POST" : "GET",
-        body: data ? JSON.stringify(data) : undefined,
-        headers: {
-            Authorization: token ? `Bearer ${token}` : undefined,
-            "Content-type": data ? 'application/json': undefined,
-            ...customHeaders
-        },
-        ...customConfig
-    }
+const apiURL = process.env.REACT_APP_API_URL;
+async function client(
+  endpoint,
+  { data, token, headers: customHeaders, ...customConfig } = {}
+) {
+  const config = {
+    method: data ? "POST" : "GET",
+    body: data ? JSON.stringify(data) : undefined,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+      "Content-type": data ? "application/json" : undefined,
+      ...customHeaders,
+    },
+    ...customConfig,
+  };
+  return window
+    .fetch(`${apiURL}/${endpoint}`, config)
+    .then(async (response) => {
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
 }
-export {client}
+export { client };
